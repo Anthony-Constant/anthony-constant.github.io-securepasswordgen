03d649c90f291810624c46de32342bb8e0c184f5
Anthony Constant <83375598+Anthony-Constant@users.noreply.github.com>
2023-11-29 17:58:29 +0000

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', initializePasswordGenerator);
 
+
 function initializePasswordGenerator() {
   const characterAmountRange = document.getElementById('characterAmountRange');
   const characterAmountNumber = document.getElementById('characterAmountNumber');
@@ -94,5 +95,24 @@ copyButton.addEventListener('click', () => {
 
 
 
+if (window.VANTA) window.VANTA.DOTS({
+  el: "html",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  showLines: false,
+  color: 0x3fddff,
+  color2: 0xff8820,
+  backgroundColor: 0x101820
+})
+
+_strk.push(function() {
+  setVanta()
+  window.edit_page.Event.subscribe( "Page.beforeNewOneFadeIn", setVanta )
+})
 
 
